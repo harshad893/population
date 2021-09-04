41eed7f865f17e1aee416512a3099d9557672aae
reddyyuvakishore1211 <79675559+reddyyuvakishore1211@users.noreply.github.com>
2021-09-04 12:38:44 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from django.db.models.functions import Length
 # Create your views here.
 from app.models import *
 def display_topic(request):
-    topics=Topic.objects.all()
+    #topics=Topic.objects.all()
+    #topics=Topic.objects.get(topic_name='cricket')
+    topics=Topic.objects.filter(topic_name='cricket')
     return render(request,'display_topic.html',context={'ts':topics})
 
 def display_webpage(request):
     webpages=Webpage.objects.all()
+    #webpages=Webpage.objects.filter(topic_name='cricket')
+    #webpages=Webpage.objects.all()[0:4:1]
+    webpages=Webpage.objects.all().order_by('name')
+    webpages=Webpage.objects.all().order_by('-name')
+    webpages=Webpage.objects.all().order_by(Length('name'))
+    webpages=Webpage.objects.all().order_by(Length('name').desc())
+    webpages=Webpage.objects.exclude(topic_name='cricket')
     return render(request,'display_webpage.html',context={'ws':webpages})
